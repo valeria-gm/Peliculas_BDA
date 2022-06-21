68dd1b17b1961e6c558cf3b2735de7b1edd3fdec
valeria-gm <68879876+valeria-gm@users.noreply.github.com>
2022-06-20 21:32:02 -0500

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,79 @@
+<?php
+$host = 'localhost';
+$usuario = 'supremo';
+$password = 'abc123muydificl';
+$bd = 'pelianalitic';
+
+$conexion = mysqli_connect($host, $usuario, $password);
+
+// Check connection
+if (!$conexion) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+  //echo "Connected successfully";
+  //echo '<pre>';
+  //print_r($conexion);
+  //echo '</pre>';
+
+$bd_conectada = mysqli_select_db($conexion, $bd);
+echo $bd_conectada;
+
+$sql = 'SELECT * FROM pelianalitic.pelicula'; #cadena de texto
+$rsql = mysqli_query($conexion, $sql); #operacion
+//print_r($rsql)
+
+// cambiar formato para usar
+$pelis = mysqli_fetch_all($rsql, MYSQLI_ASSOC);
+//print_r($songs);
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<meta charset="UTF-8">
+<head>
+<title>Mi primera página con estilo</title>
+  <link href="style.css" rel="stylesheet" type="text/css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Merriweather&display=swap" rel="stylesheet">
+</head>
+
+    <body>
+        <table>
+            <tr>
+                <th>Titulo</th>
+                <!--
+                <th>Poster Link</th>
+                <th>Año</th>
+                <th>Certificación</th>
+                <th>Duración</th>
+                <th>IMDB rating</th>
+                <th>Resumen</th>
+                <th>Calificación de la crítica</th> -->
+
+            </tr>
+            <?php
+
+            $result = mysqli_query($conexion, $sql);
+            
+            if ($result->num_rows >0) {
+                //while($row =  $rsql-> fetch_assoc()) {
+                while($row =  mysqli_fetch_assoc($result)) {
+                    echo "<tr><td>" . $row['poster_link'] . "</td><td>" . $row['titulo'] . "</td></tr>";
+                     /* . "</td><td>" . $row['anio'] . "</td><td>" . $row['certificado'] . "</td><td>" . $row['duracion'] . "</td><td>". $row['IMBD_rating'] . "</td><td>" . $row['resumen'] . "</td><td>" . $row['calificacion_critica']  . */
+                }
+            } 
+            else {
+                echo "No Results";
+            }
+            mysqli_close($conexion);
+            ?>
+
+        
+        </table>
+    </body>
+
+
+</html>
